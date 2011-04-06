665f5455b3611412ecfbb88509cc56e1335a5db2
Jeff Micklos <jeffmicklos@gmail.com>
2011-04-06 15:42:47 -0700

    added an any function

--- jQueryMissingTools.js
+++ jQueryMissingTools.js
@@ -1,9 +1,9 @@
 $.extend({
 
 	size: function(enumerableObject) {
-	
+
 		var numberOfProperties = 0;
-		
+
 		$.each(enumerableObject, function(key) {
 			if (hasOwnProperty.call(enumerableObject, key)) {
 				numberOfProperties++;
@@ -12,11 +12,11 @@ $.extend({
 
 		return numberOfProperties;
 	},
-	
+
 	values: function(map) {
 
 		var values = [];
-		
+
 		$.each(map, function(key, value) {
 			if (hasOwnProperty.call(map, key)) {
 				values.push(value);
@@ -25,29 +25,37 @@ $.extend({
 
 		return values;
 	},
-	
+
 	bind: function(functionToCall, object) {
 		return function() {
 			functionToCall.apply(object, arguments);
 		};
-		
+
 	},
-
+	
 	segment: function(collection, number) {
-
+		
 		var segmentedCollection = [];
-
+		
 		$.each(collection, function(i, item) {
 
-			if(item) {
-				segmentedCollection.push(collection.splice(0, number));
-			} else {
-				$.break;
-			}
-
+            if(item) {
+                segmentedCollection.push(collection.splice(0, number));
+            } else {
+                $.break;
+            }
+			
 		});
-
+	
 		return segmentedCollection;
+
+	},
+	
+	any: function(collection, functionToCall) {
+
+	    return $.grep(collection, function(member) {
+	           return functionToCall(member);
+	    }).length > 0
 
 	}
 
