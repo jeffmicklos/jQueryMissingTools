c6491d3210d4970ec52f71fcc73ea8406c8afd59
Jeff Micklos <jeffmicklos@gmail.com>
2011-12-09 12:42:25 -0800

    getNextIndex, getPrevIndex and docs

--- jQueryMissingTools.js
+++ jQueryMissingTools.js
@@ -26,49 +26,74 @@ $.extend({
 		return values;
 	},
 
+	/**
+	 * Divides a collection into N child pieces
+	 * @param collection {Array} a collection to divide
+	 * @param number {number} how many pieces to chop collection into
+	 * @return {Array} the newly segmented collection
+	 */
 	segment: function(collection, number) {
 	
-	    if(number === 0 || number > collection.length) {
-	        return collection;
-	    }
+		if(number === 0 || number > collection.length) {
+			return collection;
+		}
 		
 		var i;
-	    var segmentedCollection = [];
-	    var collectionClone = collection;
-	    var length = collection.length / number;
+		var segmentedCollection = [];
+		var collectionClone = collection;
+		var length = collectionClone.length / number;
 				
-	    for(i = 0; length > i; i++) {
+		for(i = 0; length > i; i++) {
 	
-	        if(number === 1) {
-	       	
-	       		segmentedCollection.push([collectionClone[i]]);
-	        
-	        } else {
-	            
-	            if(collection[i]) {
-
-	                segmentedCollection.push(collectionClone.splice(0, number));
-	  
-	            } else {
-	  
-	                 segmentedCollection.push(collectionClone);
-	                 
-	            }
-	
-	        }    		
-	       
-	    }
+			if(number === 1) {
+				segmentedCollection.push([collectionClone[i]]);
+			} else {
 			
-	    return segmentedCollection;
+				if(collection[i]) {
+					segmentedCollection.push(collectionClone.splice(0, number));
+				} else {
+			 		segmentedCollection.push(collectionClone);
+				}
+			
+			}
+		}
+			
+		return segmentedCollection;
 	
 	},
 	
 	any: function(collection, functionToCall) {
 
-	    return $.grep(collection, function(member) {
-	           return functionToCall(member);
-	    }).length > 0
+		return $.grep(collection, function(member) {
+			return functionToCall(member);
+		}).length > 0
 
+	},
+	
+	/**
+	 * Find the next index from any point of an array
+	 * If at last index, it will loop around to the front
+	 * @param collection {Array} a collection to find in
+	 * @param index {number} the starting index
+	 * @return {number} the next index from index
+	 */
+	getNextIndex: function(collection, index) {
+		
+		return (list[index++] ? index++ : 0)
+		
+	},
+	
+	/**
+	 * Find the prev index from any point of an array
+	 * If at first index, it will loop around to the back
+	 * @param collection {Array} a collection to find in
+	 * @param index {number} the starting index
+	 * @return {number} the prev index from index
+	 */
+	getPrevIndex: function(list, index) {
+		
+		return (list[index--] ? index-- : 0)
+		
 	}
 
 });
