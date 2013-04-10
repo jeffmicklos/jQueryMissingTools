02c79a698cbbfc115d2c845adc0667d99fb779d4
Jeff Micklos <jeffmicklos@gmail.com>
2013-04-09 23:05:31 -0700

    some mild refactoring, convert tabs to spaces

--- jQueryMissingTools.js
+++ jQueryMissingTools.js
@@ -1,111 +1,150 @@
 $.extend({
 
-	/**
-	 * Returns the number of (first-level) key/val pairs in an object
-	 * Think of Array.length but for Objects!
-	 * @param map {Object} an object of kv pairs to count
-	 * @return {number} the number of pairs in object
-	 */
-	size: function(map) {
+  /**
+   * Returns the number of immediate key/val pairs in an object
+   * Think Array.prototype.length but for Objects.
+   * @param obj {Object} an object of kv pairs to count
+   * @return {number} the number of pairs in object
+   */
+  size: function(obj) {
 
-		var numberOfProperties = 0;
+    var numberOfProperties = 0;
 
-		$.each(map, function(key) {
-			if (hasOwnProperty.call(map, key)) {
-				numberOfProperties++;
-			}
-		});
+    for(prop in obj) {
+      if(Object.prototype.hasOwnProperty.call(obj, prop)) {
+        numberOfProperties++;
+      }
+    }
 
-		return numberOfProperties;
-	},
+    return numberOfProperties;
 
-	/**
-	 * Returns an array of values from an object
-	 * Think of Object.keys but... values.
-	 * @param map {Object} an object to rip values from
-	 * @return {Array} an array of ever keys value
-	 */
-	values: function(map) {
+  },
 
-		var values = [];
+  /**
+   * Returns an array of values from an object
+   * Think of Object.keys but... values.
+   * @param obj {Object} an object to rip values from
+   * @return {Array} an array of ever keys value
+   */
+  values: function(obj) {
 
-		$.each(map, function(key, value) {
-			if (hasOwnProperty.call(map, key)) {
-				values.push(value);
-			}
-		});
+    if(Object.hasOwnProperty('keys')) {
+      return Object.keys(obj);
+    }
 
-		return values;
-	},
+    var keys = [];
 
-	/**
-	 * Divides a collection into N child pieces
-	 * @param collection {Array} a collection to divide
-	 * @param number {number} how many pieces to chop collection into
-	 * @return {Array} the newly segmented collection
-	 */
-	segment: function(collection, number) {
-	
-		if(number === 0 || number > collection.length) {
-			return collection;
-		}
-		
-		var i;
-		var segmentedCollection = [];
-		var collectionClone = collection;
-		var length = collectionClone.length / number;
-				
-		for(i = 0; length > i; i++) {
-	
-			if(number === 1) {
-				segmentedCollection.push([collectionClone[i]]);
-			} else {
-			
-				if(collection[i]) {
-					segmentedCollection.push(collectionClone.splice(0, number));
-				} else {
-			 		segmentedCollection.push(collectionClone);
-				}
-			
-			}
-		}
-			
-		return segmentedCollection;
-	
-	},
-	
-	any: function(collection, functionToCall) {
+    for (var prop in obj) {
+      if(Object.prototype.hasOwnProperty.call(obj, key)) {
+        keys.push(key);
+      }
+    }
 
-		return $.grep(collection, function(member) {
-			return functionToCall(member);
-		}).length > 0
+    return keys;
 
-	},
-	
-	/**
-	 * Find the next index from any point of an array
-	 * If at last index, it will loop around to the front
-	 * @param collection {Array} a collection to find in
-	 * @param index {number} the starting index
-	 * @return {number} the next index from index
-	 */
-	getNextIndex: function(collection, index) {
-		
-		return (collection[index++] ? index++ : 0)
-		
-	},
-	
-	/**
-	 * Find the prev index from any point of an array
-	 * If at first index, it will loop around to the back
-	 * @param collection {Array} a collection to find in
-	 * @param index {number} the starting index
-	 * @return {number} the prev index from index
-	 */
-	getPrevIndex: function(collection, index) {
-		
-		return (collection[index--] ? index-- : 0)
-		
-	}
+  },
+
+  /**
+   * Divides an array into N child pieces
+   * @param {Array} array a collection to divide
+   * @param {Number} sliceNumber how many pieces to chop array into
+   * @return {Array} the newly segmented collection
+   */
+  segment: function(array, sliceNumber) {
+  
+    if(sliceNumber === 0 || sliceNumber > array.length) {
+      return array;
+    }
+
+    var i,
+        segmentedArray = [],
+        arrayClone = array,
+        length = array.length / sliceNumber;
+        
+    for(i = 0; i < length; i++) {
+  
+      if(sliceNumber === 1) {
+        segmentedArray.push([array[i]]);
+      } else {
+        if(array[i]) {
+          segmentedArray.push(array.splice(0, sliceNumber));
+        } else {
+          segmentedArray.push(array);
+        }
+      }
+    }
+      
+    return segmentedArray;
+  
+  },
+
+  /**
+   * Loops over a collection, calling a validator for each member.
+   * Returns true if the validator ever returns truthy.
+   * If a validator is not given, just check truthiness.
+   * Think Ruby `.any?`
+   * @param {Array|Object} collection
+   * @param {function} validator
+   * @returns {Boolean}
+   */
+  any: function(collection, validator) {
+
+    var i = 0,
+        length,
+        validValues = [];
+
+    if(Object.prototype.toString.call(collection) === '[object Array]') {
+      length = collection.length;
+    } else {
+      length = this.size(collection);
+    }
+
+    for(;i < length; i++) {
+
+      // If a validator function is available
+      if(validator) {
+        if(validator(collection[i])) {
+          validValues.push(collection[i]);
+        }
+      // Otherwise, just check for truthiness
+      } else {
+        if(collection[i]) {
+          validValues.push(collection[i]);
+        } else {
+          return false;
+        }
+      }
+
+    }
+
+    return validValues.length > 0;
+
+  },
+  
+  /**
+   * Find the next index from any point of an array
+   * If at last index, it will loop around to the front
+   * @param collection {Array} a collection to find in
+   * @param index {number} the starting index
+   * @return {number} the next index from index
+   */
+  getNextIndex: function(array, index) {
+    
+    return (array[index++] ? index++ : 0)
+    
+  },
+  
+  /**
+   * Find the prev index from any point of an array
+   * If at first index, it will loop around to the back
+   * @param collection {Array} a collection to find in
+   * @param index {number} the starting index
+   * @return {number} the prev index from index
+   */
+  getPrevIndex: function(array, index) {
+    
+    return (array[index--] ? index-- : 0)
+    
+  }
 
 });
